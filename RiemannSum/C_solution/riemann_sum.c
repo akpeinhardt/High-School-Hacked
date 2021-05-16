175f19cf15c6e8ce931cf741620f2e7f3faf3b21
Benjamin Peinhardt <benjaminpeinhardt@gmail.com>
2021-05-16 17:12:23 -0500

    create uncommented versions of solutions

--- RiemannSum/C_solution/riemann_sum.c
+++ RiemannSum/C_solution/riemann_sum.c
@@ -1,50 +1,31 @@
 #include <stdio.h>
 
-// 'enum' is short for Enumeration. Enums in c are a handy way
-// to create a finite list of options. One of the most common ways
-// enums are used in C is to create error types.
 enum Method {
     Left,
     Right,
     Midpoint
 };
 
-// This is the function we plan to perform our riemann sum on.
-// In this case, f(x) = x^2
 double f(double x) {
     return x * x;
 }
 
-// This function actually calculates the riemann sum, and returns a floating point answer
 double calculate(double x0, double x1, int n, enum Method method) {
     
-    // The width of the rectangle = the width of the graph / the number of rectangles
-    // if we do a riemann sum with 7 rectangles from x = 0 to x = 21,
-    // then each rectangle is 3 wide
     double interval = (x1 - x0) / (double) n;
-    
     
     double evalAt;
     if (method == Left) {
-        // for a left riemann sum, we start at the beginning
         evalAt = x0;
     } else if (method == Midpoint) {
-        // for a midpoint sum, we move halfway into the rectangle
         evalAt = x0 + (interval / 2.0);
     } else if (method == Right) {
-        // for a right sum, we move to the right side of the rectangle
         evalAt = x0 + interval;
     }
 
-    // We are going to use this variable to add up the areas of all the rectangles
     double sum = 0;
-
-    // we add up n rectangles so we iterate n times
     for (int i = 0; i < n; i++) {
-
-        // Evaluate the function at the point and add the result to our total
         sum += f(evalAt) * interval;
-        // Go to the nect rectangle
         evalAt += interval;
     }
 
@@ -53,20 +34,8 @@ double calculate(double x0, double x1, int n, enum Method method) {
 
 int main() {
 
-    // Most introductory C courses do not cover testing, and C does not have
-    // a defacto winner for testing frameworks. So in the spirit of reliving
-    // college, we will use a bunch of print statements instead.
-
-    // Evaluate x^2 from 0..3 with n = 3 rectangles as a Left Riemann sum
-    // should print 5
     printf("From the left: %lf\n", calculate(0, 3, 3, Left));
-
-    // Evaluate x^2 from 0..3 with n = 3 rectangles as a Right Riemann sum
-    // should print 14
     printf("From the right: %lf\n", calculate(0, 3, 3, Right));
-
-    // Evaluate x^2 from 0..3 with n = 3 rectangles as a Midpoint Riemann sum
-    // should print 8.75
     printf("From the midpoint: %lf\n", calculate(0, 3, 3, Midpoint));
 
     return 0;
--- RiemannSum/rust_solution/src/lib.rs
+++ RiemannSum/rust_solution/src/lib.rs
@@ -1,49 +1,18 @@
-/// This module contains a structure for calculating left, right, and midpoint riemann sums.
-
-// "struct" is short for structure. A struct in rust is basically
-// just a big glob information. If you're coming from another programming language
-// this is often called an "object" or a "record"
-//
-// For our riemann sum, the information
-// we need to solve the problem is:
-//
-// A function to approximate: func
-// a starting point: x0
-// an ending point: xf
-// the number of rectangles to use: n
-//
-// We also want to know which kind of riemann sum we are doing (left, right, midpoint)
-// For that we use our own list of possibilities called method
-//
-// The 'pub' keyword just means public. It's there so other people can use our struct
 pub struct RiemannSum {
-    func: fn(f64) -> f64, // This is a pointer to a function which takes a 64 bit floating point number and returns a 64 bit floating point number
+    func: fn(f64) -> f64,
     x0: f64,
     xf: f64,
     n: u64,
     method: Method,
 }
 
-// "enum" is short for enumeration. Enums in programming are often a
-// convenient way to define a set number of options/types. In rust, enums
-// are much more powerful than this, but all we need is a simple one
 pub enum Method {
     Right,
     Left,
     Midpoint,
 }
 
-// "impl" is short for Implementation. This is where we can write behavior associated with our big
-// globs of information. You can also write behavior that is associated with more than one kind of struct
-// using "traits", but we don't need that for our calculator.
 impl RiemannSum {
-    // This function is what's often called a 'constructor'. It takes in all the info we need
-    // and returns 'Self" which is just an easy way of writing 'RiemannSum'. If you replace the word Self
-    // with RiemannSum, this function will still work. This one just passes along the info,
-    // but constructors can be used to do calculations for the user as well.
-    // You don't have to have a constructor in Rust, instead you can type the name of the struct and
-    // the data all in squiggly braces (which is exactly what our constructor does), but they are
-    // often convenient.
     pub fn new(func: fn(f64) -> f64, x0: f64, xf: f64, n: u64, method: Method) -> Self {
         RiemannSum {
             func,
@@ -54,41 +23,18 @@ impl RiemannSum {
         }
     }
 
-    // This is where the magic happens. This function uses all the information in our struct to calculate a riemann sum!
-    // The 'self' keyword is our way to access the information stored in our struct
     pub fn calculate(&self) -> f64 {
-        // The width of the rectangles = the width of the graph divided by the number of rectangles
-        // i.e. If we squeeze 7 rectangles between x=0 and x=21, each rectangle has a width of 3
         let interval = (self.xf - self.x0) / (self.n as f64);
-
-        // We are going to add the area of each rectangle to this variable, and return it at the end
         let mut total_area = 0.0;
-
-        // For a left riemann sum, we start at the very left of the first rectangle
-        // For a middle riemann sum, we start in the middle of the first rectangle
-        // For a right riemann sum, we start to the right of the first rectangle
         let mut eval_at = match self.method {
             Method::Left => self.x0,
             Method::Midpoint => self.x0 + (interval / 2.0),
             Method::Right => self.x0 + interval,
         };
-
-        // We need to add up n rectangles, so we do a loop counting from 0..(n-1). Notice the first term is included but the second one is excluded
         for _ in 0..self.n {
-            // The area of a rectangle is its height (func(eval_at)) * its width (interval)
-            // The weird (self.func)(eval_at) is actually calling the function func with eval_at as the input
-            // We store a pointer to the function in the struct, but if we write self.func(eval_at), the
-            // compiler thinks we're calling a method called func. The parenthesis around (self.func) make it clear
-            // that func is an attribute rather than a method.
             total_area += (self.func)(eval_at) * interval;
-
-            // After calculating the area of our rectangle, we increase x by the width of the ractangle to take us to the next one
             eval_at += interval;
         }
-
-        // In rust, any variable that isn't supressed with a semi-colon is returned.
-        // This is because everything in rust is an "expression". You can read more about this
-        // in the rust book.
         total_area
     }
 }
@@ -123,5 +69,102 @@ mod tests {
 
         // Sum should equal (0.5)^2 + (1.5)^2 + (2.5)^2 = 8.75
         assert_eq!(sum, 8.75);
+    }
+}
+
+mod commented_solution {
+    /// This module contains a structure for calculating left, right, and midpoint riemann sums.
+
+    // "struct" is short for structure. A struct in rust is basically
+    // just a big glob information. If you're coming from another programming language
+    // this is often called an "object" or a "record"
+    //
+    // For our riemann sum, the information
+    // we need to solve the problem is:
+    //
+    // A function to approximate: func
+    // a starting point: x0
+    // an ending point: xf
+    // the number of rectangles to use: n
+    //
+    // We also want to know which kind of riemann sum we are doing (left, right, midpoint)
+    // For that we use our own list of possibilities called method
+    //
+    // The 'pub' keyword just means public. It's there so other people can use our struct
+    pub struct RiemannSum {
+        func: fn(f64) -> f64, // This is a pointer to a function which takes a 64 bit floating point number and returns a 64 bit floating point number
+        x0: f64,
+        xf: f64,
+        n: u64,
+        method: Method,
+    }
+
+    // "enum" is short for enumeration. Enums in programming are often a
+    // convenient way to define a set number of options/types. In rust, enums
+    // are much more powerful than this, but all we need is a simple one
+    pub enum Method {
+        Right,
+        Left,
+        Midpoint,
+    }
+
+    // "impl" is short for Implementation. This is where we can write behavior associated with our big
+    // globs of information. You can also write behavior that is associated with more than one kind of struct
+    // using "traits", but we don't need that for our calculator.
+    impl RiemannSum {
+        // This function is what's often called a 'constructor'. It takes in all the info we need
+        // and returns 'Self" which is just an easy way of writing 'RiemannSum'. If you replace the word Self
+        // with RiemannSum, this function will still work. This one just passes along the info,
+        // but constructors can be used to do calculations for the user as well.
+        // You don't have to have a constructor in Rust, instead you can type the name of the struct and
+        // the data all in squiggly braces (which is exactly what our constructor does), but they are
+        // often convenient.
+        pub fn new(func: fn(f64) -> f64, x0: f64, xf: f64, n: u64, method: Method) -> Self {
+            RiemannSum {
+                func,
+                x0,
+                xf,
+                n,
+                method,
+            }
+        }
+
+        // This is where the magic happens. This function uses all the information in our struct to calculate a riemann sum!
+        // The 'self' keyword is our way to access the information stored in our struct
+        pub fn calculate(&self) -> f64 {
+            // The width of the rectangles = the width of the graph divided by the number of rectangles
+            // i.e. If we squeeze 7 rectangles between x=0 and x=21, each rectangle has a width of 3
+            let interval = (self.xf - self.x0) / (self.n as f64);
+
+            // We are going to add the area of each rectangle to this variable, and return it at the end
+            let mut total_area = 0.0;
+
+            // For a left riemann sum, we start at the very left of the first rectangle
+            // For a middle riemann sum, we start in the middle of the first rectangle
+            // For a right riemann sum, we start to the right of the first rectangle
+            let mut eval_at = match self.method {
+                Method::Left => self.x0,
+                Method::Midpoint => self.x0 + (interval / 2.0),
+                Method::Right => self.x0 + interval,
+            };
+
+            // We need to add up n rectangles, so we do a loop counting from 0..(n-1). Notice the first term is included but the second one is excluded
+            for _ in 0..self.n {
+                // The area of a rectangle is its height (func(eval_at)) * its width (interval)
+                // The weird (self.func)(eval_at) is actually calling the function func with eval_at as the input
+                // We store a pointer to the function in the struct, but if we write self.func(eval_at), the
+                // compiler thinks we're calling a method called func. The parenthesis around (self.func) make it clear
+                // that func is an attribute rather than a method.
+                total_area += (self.func)(eval_at) * interval;
+
+                // After calculating the area of our rectangle, we increase x by the width of the ractangle to take us to the next one
+                eval_at += interval;
+            }
+
+            // In rust, any variable that isn't supressed with a semi-colon is returned.
+            // This is because everything in rust is an "expression". You can read more about this
+            // in the rust book.
+            total_area
+        }
     }
 }
